8ab7c5a311a1f666fc5f70cc41d073c33fef9ecc
as-segaf <ziyanassegaf20@gmail.com>
2021-05-01 11:13:03 +0700

    handle login process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        return $this->authService = $authService;
+    }
+
     public function login()
     {
         return view('auth.login');
+    }
+
+    public function loginPost(LoginRequest $request)
+    {
+        try {
+            $user = $this->authService->login($request);
+        } catch (\Throwable $th) {
+            return redirect('/login')->with('error', $th->getMessage());
+        }
+
+        return redirect('/home');
     }
 }
