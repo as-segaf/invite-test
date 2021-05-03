193a9e5ff545ea0b5f993430e72541feda8f4bf0
as-segaf <ziyanassegaf20@gmail.com>
2021-05-03 13:53:13 +0700

    create invitation model & migration

---
+++ database/migrations/2021_05_03_064835_create_invitations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->string('additional_info');
+            $table->date('event_date');
+            $table->string('status');
+            $table->foreignId('sent_by')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitations');
+    }
+}
