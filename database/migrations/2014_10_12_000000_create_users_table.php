f71e5853fd1b59e8452bfb4400685cdb98f6f7bf
as-segaf <ziyanassegaf20@gmail.com>
2021-05-10 09:25:43 +0700

    upodate migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('is_admin')->default(0);
+            $table->string('google_Id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
