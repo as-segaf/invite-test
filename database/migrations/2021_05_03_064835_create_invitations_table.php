b3a5d9077868c663f3bbda2cd7811ea80fb9f06b
as-segaf <ziyanassegaf20@gmail.com>
2021-05-20 10:15:33 +0700

    change column to nullable

--- database/migrations/2021_05_03_064835_create_invitations_table.php
+++ database/migrations/2021_05_03_064835_create_invitations_table.php
@@ -16,7 +16,7 @@ class CreateInvitationsTable extends Migration
         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
             $table->string('event_name');
-            $table->string('additional_info');
+            $table->string('additional_info')->nullable();
             $table->date('event_date');
             $table->string('status');
             $table->foreignId('sent_by')->constrained('users');
