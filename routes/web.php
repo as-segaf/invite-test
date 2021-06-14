798c1f12430ac8bccae17fbf277375421cb254b7
as-segaf <ziyanassegaf20@gmail.com>
2021-06-14 09:59:10 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,6 @@ Route::middleware('auth')->group(function() {
     });
 
     Route::middleware('user')->group(function() {
-        Route::resource('invitation', InvitationController::class)->except('create', 'show', 'edit');
+        Route::resource('invitation', InvitationController::class)->except('show', 'edit');
     });
 });
