0229ef965a31b7adc4ad76bb8f569ada31e1dae7
as-segaf <ziyanassegaf20@gmail.com>
2021-06-14 09:10:43 +0700

    update invitation table

--- database/migrations/2021_05_03_064835_create_invitations_table.php
+++ database/migrations/2021_05_03_064835_create_invitations_table.php
@@ -15,9 +15,20 @@ class CreateInvitationsTable extends Migration
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
-            $table->string('event_name');
-            $table->string('additional_info')->nullable();
-            $table->date('event_date');
+            $table->string('full_name');
+            $table->string('nick_name');
+            $table->string('wa_number');
+            $table->string('organization_type');
+            $table->string('organization_name');
+            $table->string('invite_vos_as');
+            $table->string('event_type');
+            $table->timestamp('event_date');
+            $table->timestamp('event_date2')->nullable();
+            $table->string('event_duration')->nullable();
+            $table->string('event_place');
+            $table->text('event_detail');
+            $table->string('participant');
+            $table->text('additional_note')->nullable();
             $table->string('status');
             $table->foreignId('sent_by')->constrained('users');
             $table->timestamps();
