95dae25f470238dccc7c0cce1fdca930c4763b24
as-segaf <ziyanassegaf20@gmail.com>
2021-06-15 10:21:36 +0700

    create mailable

---
+++ app/Mail/InvitationUpdateMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InvitationUpdateMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $invitation; 
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($invitation)
+    {
+        $this->invitation = $invitation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Update for your invitation from VOS')->view('emails.invitationUpdate');
+    }
+}
